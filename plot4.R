20d50fb7dc43f5adfc7870b4931ffe7cedf51d9b
Tanner Prestegard (desktop) <tprestegard@gmail.com>
2015-05-06 18:03:17 -0500

    update to plotting code

--- plot4.R
+++ plot4.R
@@ -7,11 +7,11 @@ plot4 <- function() {
     ## determined by manually looking at the data.
     col_class = c("character","character","numeric","numeric","numeric",
                   "numeric","numeric","numeric","numeric")
-    data <- read.table("household_power_consumption.txt", header=TRUE,
+    data <- read.table("../household_power_consumption.txt", header=TRUE,
                         sep=";", skip=66636, nrows=2880, stringsAsFactors=FALSE)
     
     ## Need to read in the column names too since we skipped rows.
-    header <- read.table("household_power_consumption.txt", header=FALSE,
+    header <- read.table("../household_power_consumption.txt", header=FALSE,
                          nrows=1, sep=";", stringsAsFactors=FALSE)
     colnames(data) <- unlist(header)
     
